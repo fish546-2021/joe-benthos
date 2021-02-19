5a47aaf975a3cc6fc0b0214170d541634d8cf8ea
jdduprey <joe.d.duprey@gmail.com>
2021-02-19 12:52:54 -0800

    comments to viz code

--- analysis/02-dataviz.R
+++ analysis/02-dataviz.R
@@ -9,25 +9,29 @@ library(phyloseq); packageVersion("phyloseq")
 library(Biostrings); packageVersion("Biostrings")
 library(ggplot2); packageVersion("ggplot2")
 
+#higher taxa table
 higher_tax <- read.csv(here('asv-output','higher_taxonomy.csv'))
 
+#table of class and order sums
 class_order_table <- table(higher_tax$class, higher_tax$order)
 class_table <- (table(higher_tax$class))
 
+#convert to dataframe, sort by descending freq
 class_order_df <- class_order_table %>%
   as.data.frame() %>%
   arrange(desc(Freq)) %>%
   top_n(20) 
 
+#rename cols
 class_order_df <- rename(class_order_df, Var1 = "class", Var2 = "order")
 
-  
+#sanity check for class totals with class table   
 class_df <- class_table %>%
   as.data.frame() %>%
   arrange(desc(Freq)) %>%
   top_n(100)
 
-# basic treemap
+#treemap code (could make this interactive weidget for less clutter?)
 jpeg("rough_treemap.jpeg", width = 700, height = 700)
 
 rough_treemap <- treemap(class_order_df,

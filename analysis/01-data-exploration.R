23a12088765a51e9ae83c4678ea7c1321ad707e8
jdduprey <joe.d.duprey@gmail.com>
2021-02-19 12:39:52 -0800

    very rough attempt at visualizing the output data

--- analysis/01-data-exploration.R
+++ analysis/01-data-exploration.R
@@ -1,9 +1,10 @@
 library('here')
 library('dplyr')
+library('stringr')
 
 # load Rarmon's data
-ASV_table <- read.csv(here('data','ASV_table_all_together.csv'))
-taxonomy_table <- read.csv(here('data','all.taxonomy.20190130.csv'))
+ASV_table <- read.csv(here('asv-output','ASV_table_all_together.csv'))
+taxonomy_table <- read.csv(here('asv-output','all.taxonomy.20190130.csv'))
 
 # NA's in ASV_table hash? 
 sum(is.na(ASV_table$Hash))
@@ -13,8 +14,39 @@ sum(is.na(taxonomy_table$Hash))
 ASV_tax <- inner_join(ASV_table, taxonomy_table)
 
 unique(ASV_tax$sample)
-sum(unique(ASV_tax$sample))
 
 #OYSTERS filter
 oysters <- ASV_tax %>%
-  filter(genus %in% c('Ostrea','Crassostrea'))
+  #filter(genus %in% c('Ostrea','Crassostrea'))
+  filter(genus %in% c('Ostrea'))
+
+oysters_str <- ASV_tax %>%
+  filter(str_detect(genus, 'Ostrea')) #these filters do the same thing 
+
+# balanus filter
+balanus <- ASV_tax %>%
+  filter(str_detect(genus, 'Balanus'))
+
+# STAR FEEESH
+ starfeesh <- ASV_tax %>%
+   filter(str_detect(family, "Asteriidae"))
+
+# how bout octopus?
+ salmon <- ASV_tax %>%
+   filter(str_detect(family, "Salmonidae"))
+
+# geoduck
+ geoduck <- ASV_tax %>%
+   filter(str_detect(family, 	"Hiatellidae"))
+
+# LL filter
+LL_oysters <- oysters %>%
+  filter(str_detect(sample, "LL")) 
+
+#Sample test filter 
+by_sample_test <- ASV_tax %>% 
+  filter(str_detect(sample, "LL_201703"))
+
+#taxonomy no.na
+tax_nona <- taxonomy_table[!is.na(taxonomy_table$species), ]
+  

6fec40d6ee5a237e24e885c877d7bd8627344036
jdduprey <joe.d.duprey@gmail.com>
2021-03-09 14:08:28 -0800

    completed insect taxa assignment on subsetted data wooo

---
+++ analysis/05-insect-taxa.R
@@ -0,0 +1,41 @@
+library(insect)
+library(here)
+library(tidyverse)
+library(reshape2)
+
+# uncomment if file is needed
+#download.file("https://www.dropbox.com/s/dvnrhnfmo727774/classifier.rds?dl=1", 
+#              destfile = "classifier.rds", mode = "wb")
+
+# insect tutorial 
+# https://cran.r-project.org/web/packages/insect/vignettes/insect-vignette.html
+
+## read in the example seqtab.nochim ASV table
+data(samoa)
+ASV_tab <- read.csv(here('./01-demultiplexed','ASV_table.csv'))
+hash_key <- read.csv(here('./01-demultiplexed','hash_key.csv'))
+
+seq_tab <- inner_join(ASV_tab, hash_key)
+seq_tab <- select(seq_tab, -c(Hash))
+
+seq_tab = seq_tab %>% 
+  spread(Sequence, nReads) %>%
+  replace(is.na(.), 0)
+
+
+## get sequences from table column names
+x <- char2dna(colnames(seq_tab))
+## name the sequences sequentially
+names(x) <- paste0("ASV", seq_along(x))
+## optionally remove column names that can flood the console when printed
+# colnames(seq_tab) <- NULL 
+
+classifier <- readRDS("classifier.rds")
+classifier
+names(attributes(classifier))
+
+longDF <- classify(x, classifier, threshold = 0.8)
+
+longDF <- cbind(longDF, t(seq_tab))
+
+write_csv(longDF, './all.the.useful.tables/01subset.taxa.table.csv')
